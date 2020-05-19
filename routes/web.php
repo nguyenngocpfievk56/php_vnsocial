aba2266cf1b6ef10109102bf388c8f38134785e1
Nguyen Huu Ngoc <nguyen.huu.ngoc@elife.co.jp>
2020-05-19 22:21:38 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/screen2', function () {
+    return view('screen2');
+});
+
+Route::get('/screen3', function () {
+    return view('screen3');
+});
+
+Route::get('/screen4', function () {
+    return view('screen4');
+});
